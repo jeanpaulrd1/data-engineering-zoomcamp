24a4ac43f8a89042ca8f80bf1f21a696909d9497
jeanpaulrd1 <jeanpaulrd1@gmail.com>
2024-01-04 15:53:24 -0500

    Adding terraform demo with variables

--- week_1/1_terraform_gcp/main.tf
+++ week_1/1_terraform_gcp/main.tf
@@ -1,44 +1,38 @@
 terraform {
-    required_providers {
-      google = {
-        source = "hashicorp/google"
-        version = "5.6.0"
-      }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
     }
+  }
 }
 
 provider "google" {
-    project = "airy-digit-410117"
-    region = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "data-lake-bucket-410117"
-  location      = "US"
 
-  # Optional, but recommended settings:
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
 
-  versioning {
-    enabled     = true
-  }
 
   lifecycle_rule {
-    action {
-      type = "Delete"
-    }
     condition {
-      age = 30  // days
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
     }
   }
-
-  force_destroy = true
 }
 
 
-/*resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "data-lake-bucket-410117"
-  project    = "airy-digit-410117"
-  location   = "US"
-}*/
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
