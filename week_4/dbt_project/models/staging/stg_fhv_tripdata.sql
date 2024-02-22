cbc1e5c09f3cd665b329dfdda103e8f39ad10a2d
Jean Paul Rodriguez Farfan <jeanpaulrd1@gmail.com>
2024-02-22 02:34:41 +0000

    -

--- week_4/dbt_project/models/staging/stg_fhv_tripdata.sql
+++ week_4/dbt_project/models/staging/stg_fhv_tripdata.sql
@@ -5,7 +5,7 @@
   where EXTRACT(YEAR FROM pickup_datetime) = 2019 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
+{% if var('is_test_run', default=false) %}
 
   limit 100
 
